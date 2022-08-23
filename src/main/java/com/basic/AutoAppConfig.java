6584e291dd92fd060fd47d1110494d65a9f7261f
Jeremy Kim <milky0610.mk@gmail.com>
2022-08-24 00:31:34 +0900

    섹션 6. 컴포넌트 스캔

---
+++ src/main/java/com/basic/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+import com.basic.member.MemberRepository;
+import com.basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "com.basic.member", // 지정하지 않으면 AutoAppConfig 클래스가 위치한 패키지 이하를 스캔
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    };
+}
--- src/main/java/com/basic/discount/RateDiscountPolicy.java
+++ src/main/java/com/basic/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.basic.discount;
 
 import com.basic.member.Grade;
 import com.basic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
